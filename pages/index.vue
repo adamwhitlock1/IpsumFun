2fc767a99ede6830849220035301005d19540511
Adam Whitlock <adam.whitlock627@gmail.com>
2019-11-06 06:46:03 -0700

    Update index.vue
    reformat layout slightly, add copy to clipboard

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="mx-auto container object-center">
     <div class="flex align-middle align-items-center align-content-center">
       <div class="w-full">
-        <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 mt-8">
+        <div class="bg-white shadow rounded px-8 pt-6 pb-8 mb-4 mt-8">
           <h1 class="text-2xl text-blue-600 font-mono font-hairline mb-4">
             Ipsum Fun
           </h1>
@@ -16,7 +16,7 @@
               <input
                 id="paragraphs"
                 v-model="paragraphs"
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                class="shadow appearance-none border rounded w-full py-2 px-3 border-gray-100 hover:border-gray-500 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 name="paragraphs"
                 type="text"
               />
@@ -31,7 +31,7 @@
                 <select
                   id="subject"
                   v-model="selectedCategory"
-                  class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                  class="block appearance-none w-full bg-white border border-gray-100 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
                 >
                   <option
                     v-for="(option, index) in categories"
@@ -57,7 +57,7 @@
               </div>
             </div>
           </div>
-          <div class="mb-6">
+          <div class="mb-6 px-1">
             <p class="block text-gray-700 text-sm font-bold mb-2">
               paragraph length
             </p>
@@ -89,7 +89,7 @@
             />
             <label for="long" class="py-2 pl-1 pr-3 text-gray-700">long</label>
           </div>
-          <div class="flex items-center justify-between">
+          <div class="flex px-1">
             <button
               class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="button"
@@ -97,6 +97,14 @@
             >
               Get Some Ipsum
             </button>
+            <button
+              class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 ml-2 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+              @click="copyIpsum"
+            >
+              Copy To Clipboard
+            </button>
+            <span class="py-2 px-4">{{ copyConfirm }}</span>
           </div>
         </div>
       </div>
@@ -116,7 +124,8 @@ export default {
       paragraphLength: 'medium',
       result: '',
       categories: [],
-      selectedCategory: 'photo'
+      selectedCategory: 'photo',
+      copyConfirm: ''
     }
   },
   mounted() {
@@ -124,6 +133,32 @@ export default {
     this.getIpsum()
   },
   methods: {
+    copyIpsum() {
+      console.log('ipsum')
+      const el = document.createElement('textarea') // Create a <textarea> element
+      el.value = this.result // Set its value to the string that you want copied
+      el.setAttribute('readonly', '') // Make it readonly to be tamper-proof
+      el.style.position = 'absolute'
+      el.style.left = '-9999px' // Move outside the screen to make it invisible
+      document.body.appendChild(el) // Append the <textarea> element to the HTML document
+      const selected =
+        document.getSelection().rangeCount > 0 // Check if there is any content selected previously
+          ? document.getSelection().getRangeAt(0) // Store selection if found
+          : false // Mark as false to know no selection existed before
+      el.select() // Select the <textarea> content
+      document.execCommand('copy') // Copy - only works as a result of a user action (e.g. click events)
+      document.body.removeChild(el) // Remove the <textarea> element
+      if (selected) {
+        // If a selection existed before copying
+        document.getSelection().removeAllRanges() // Unselect everything on the HTML document
+        document.getSelection().addRange(selected) // Restore the original selection
+      }
+
+      this.copyConfirm = 'Copied!'
+      setTimeout(() => {
+        this.copyConfirm = ''
+      }, 750)
+    },
     getIpsum() {
       this.$axios
         .get(
